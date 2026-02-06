0ed73fe0b66b1ebe49f038e247f41e1bfed0a867
NathanLuna15 <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>
2026-02-06 10:26:09 -0300

    não sei o que fazer

--- app.js
+++ app.js
@@ -25,9 +25,29 @@ entradaDeDados.question("digite o nome do cliente: ", function(nome){
 
                     }else if(valorProduto <= 0 || taxaDeJuros <= 0 || prarcelas <= 0){
                         console.log("algums dos valores não foram preenchidos")
+                    
+                    }else if(isNaN(valorProduto) || isNaN(taxaDeJuros) || isNaN(prarcelas)){
+                        console.log("somente numeros são permitidos, algums dos campos estão errados")
+                    
+                    }else {
+                        let taxa = Number(taxaDeJuros) / 100;
+                        //C * (1 + i) + (n * n) ????????????????????????
+                        let valorTotal = Number(valorProduto) * (1 + Number(taxaDeJuros)) + (Number(prarcelas) * Number(prarcelas))
+                   
+                        console.log(valorTotal)
+                        console.log("\n");
+                        console.log(`
+                                ******************* [Viva Moda] *******************\n
+                            Muito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.\n
+                            A compra do produto ${nomeProduto}, tem um valor de: ${valorProduto}.\n
+                            A sua compra será parcelada em ${taxa} vezes e o Sr(a) pagará: ${valorTotal}.\n
+                            O acréscimo realizado ao valor de: ${valorProduto} será de xxxxxxxxxx.\n
+                            Muito obrigado por escolher a [Viva Moda].\n
+                                 *******************************************************
+
+
+                            `);
                     }
-
-
             })
           })
         })
